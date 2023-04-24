c0bf28d894e733db8f16ca3b323aecec9af0aaf3
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2023-04-25 01:14:58 +0530

    install vs code

--- configure.sh
+++ configure.sh
@@ -1,5 +1,19 @@
 apt-get update
 apt-get upgrade
 
-# Install wmctrl for keybinding shortcuts (show desktop)
-apt-get install wmctrl
+# Install brave browser
+sudo apt install curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+# Install vs code
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+echo "VS Code installed successfully"
+
+# Configure keybindings
